715b79bb23aca91f076c7670ccd7e0c9b8b5b7a1
fixcybj <valentin.pravtchev@cariad.technology>
2025-12-08 13:20:22 +0100

    bugfixed pipeline

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setuptools.setup(
     version="0.0.1",
     description="Starter code.",
     author="Valentin Pravtchev (valentin.pravtchev@cariad.technology)",
+    packages=setuptools.find_packages(include=["starter", "starter.*"]),
 )
